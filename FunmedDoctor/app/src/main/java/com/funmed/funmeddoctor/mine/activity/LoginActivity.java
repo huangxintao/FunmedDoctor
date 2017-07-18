3fbdb8b4d6539535cbb0dfe984510e5f51a5e60d
huangxintao <1024553047@qq.com>
2017-07-18 16:11:08 +0800

    修改

--- FunmedDoctor/app/src/main/java/com/funmed/funmeddoctor/mine/activity/LoginActivity.java
+++ FunmedDoctor/app/src/main/java/com/funmed/funmeddoctor/mine/activity/LoginActivity.java
@@ -2,11 +2,20 @@ package com.funmed.funmeddoctor.mine.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.funmed.funmeddoctor.R;
+import com.funmed.funmeddoctor.home.activity.MainTabActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.murphy.common.base.BaseActivity;
 
 /**
@@ -16,6 +25,16 @@ import me.murphy.common.base.BaseActivity;
 public class LoginActivity extends BaseActivity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
+    @Bind(R.id.et_username)
+    EditText etUsername;
+    @Bind(R.id.et_password)
+    EditText etPassword;
+    @Bind(R.id.btn_login)
+    Button btnLogin;
+    @Bind(R.id.tv_forget_password)
+    TextView tvForgetPassword;
+    @Bind(R.id.tv_register_now)
+    TextView tvRegisterNow;
 
     @Override
     public int getLayoutId() {
@@ -37,5 +56,26 @@ public class LoginActivity extends BaseActivity {
         toolbar.setTitle("登录");
         setSupportActionBar(toolbar);
         SetTranslanteBar();
+    }
+
+
+    @OnClick({R.id.btn_login, R.id.tv_forget_password, R.id.tv_register_now})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                doLogin();
+                break;
+            case R.id.tv_forget_password:
+                startActivity(RegisterActivity.class);
+                break;
+            case R.id.tv_register_now:
+                startActivity(RegisterActivity.class);
+                break;
+        }
+    }
+
+    private void doLogin() {
+        startActivity(MainTabActivity.class);
+        finish();
     }
 }
