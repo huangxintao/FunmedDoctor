c72f2bd23284d981d64bbca03c275420ebbd807f
huangxintao <1024553047@qq.com>
2017-10-25 17:45:58 +0800

    修改

---
+++ FunmedDoctor/app/src/main/java/com/funmed/funmeddoctor/home/activity/GuideActivity.java
@@ -0,0 +1,10 @@
+package com.funmed.funmeddoctor.home.activity;
+
+import me.murphy.common.base.BaseActivity;
+
+/**
+ * Created by tony on 2017/10/25.
+ */
+
+public class GuideActivity extends BaseActivity {
+}
---
+++ FunmedDoctor/app/src/main/java/com/funmed/funmeddoctor/scientific/activity/SecondNormalDetectionActivitiy.java
@@ -0,0 +1,10 @@
+package com.funmed.funmeddoctor.scientific.activity;
+
+import me.murphy.common.base.BaseActivity;
+
+/**
+ * Created by tony on 2017/10/25.
+ */
+
+public class SecondNormalDetectionActivitiy extends BaseActivity {
+}
